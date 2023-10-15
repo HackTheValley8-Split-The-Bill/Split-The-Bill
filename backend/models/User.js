b1582dbe0353acee8ce5c296b528788f3d66fc99
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2023-10-14 20:02:44 -0400

    Changed indent spaces from `2` to `4`

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,28 +2,28 @@ import mongoose from '../db/dataBase.js';
 const { Schema, ObjectId } = mongoose;
 
 const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  friends: [{
-    type: ObjectId,
-    ref: 'User'
-  }],
-  groups: [{
-    type: ObjectId,
-    ref: 'Group'
-  }],
-  balance: {
-    type: Number,
-    required: true,
-    default: 0
-  }
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    friends: [{
+        type: ObjectId,
+        ref: 'User'
+    }],
+    groups: [{
+        type: ObjectId,
+        ref: 'Group'
+    }],
+    balance: {
+        type: Number,
+        required: true,
+        default: 0
+    }
 });
 
 const User = await mongoose.model("User", userSchema);
--- backend/routers/user.js
+++ backend/routers/user.js
@@ -4,116 +4,116 @@ import { createUser } from "../modules/user.js";
 export const userRouter = express.Router();
 
 userRouter.get("/", async function (req, res) {
-  console.info('You\'ve reached the user router!');
-  res.status(200).end('Hello World!');
+    console.info('You\'ve reached the user router!');
+    res.status(200).end('Hello World!');
 });
 
 // Login
 userRouter.post("/login", async function (req, res) {
-  const { name, password } = req.body;
+    const { name, password } = req.body;
 
-  // Validate name and password
-  if (!name || !password) {
-    if (!name) {
-      res.status(401).json({ "error": "Invalid name" });
+    // Validate name and password
+    if (!name || !password) {
+        if (!name) {
+            res.status(401).json({ "error": "Invalid name" });
+        }
+        if (!password) {
+            res.status(401).json({ "error": "Invalid password" });
+        }
     }
-    if (!password) {
-      res.status(401).json({ "error": "Invalid password" });
-    }
-  }
 
-  // Check if user exists
-  else {
-    const user = await User.findOne({ "name": name });
-    if (user) {
-      if (user.password === password) {
-        // TODO: Calculate balance when logging in
-        console.info('user', user);
-        res.status(200).json({ "id": user._id.toString() });
-      } else {
-        res.status(401).json({ "error": "Invalid password" });
-      }
-    } else {
-      res.status(401).json({ "error": "Invalid user" });
+    // Check if user exists
+    else {
+        const user = await User.findOne({ "name": name });
+        if (user) {
+            if (user.password === password) {
+                // TODO: Calculate balance when logging in
+                console.info('user', user);
+                res.status(200).json({ "id": user._id.toString() });
+            } else {
+                res.status(401).json({ "error": "Invalid password" });
+            }
+        } else {
+            res.status(401).json({ "error": "Invalid user" });
+        }
     }
-  }
 
 })
 
 // Register a new user
 userRouter.post("/register", async function (req, res) {
-  const { name, password } = req.body;
+    const { name, password } = req.body;
 
-  // Validate name and password
-  if (!name || !password) {
-    if (!name) {
-      res.status(401).json({ "error": "Invalid name" });
+    // Validate name and password
+    if (!name || !password) {
+        if (!name) {
+            res.status(401).json({ "error": "Invalid name" });
+        }
+        if (!password) {
+            res.status(401).json({ "error": "Invalid password" });
+        }
     }
-    if (!password) {
-      res.status(401).json({ "error": "Invalid password" });
-    }
-  }
 
-  // Create user
-  else {
-    try {
-      const result = await createUser(name, password);
-      console.info('result', result);
-      res.status(200).json({ "user_id": result._id });
-    } catch (e) {
-      console.error('e', e);
-      if (e.code === 11000) {
-        res.status(401).json({ "error": "User already exists" });
-      } else {
-        res.status(500).json({ "error": `Unknown server error: ${e}` });
-      }
+    // Create user
+    else {
+        try {
+            const result = await createUser(name, password);
+            console.info('result', result);
+            res.status(200).json({ "user_id": result._id });
+        } catch (e) {
+            console.error('e', e);
+            if (e.code === 11000) {
+                res.status(401).json({ "error": "User already exists" });
+            } else {
+                res.status(500).json({ "error": `Unknown server error: ${e}` });
+            }
+        }
     }
-  }
 
 });
 
 // Get user total balance
 userRouter.get("/balance", async function (req, res) {
-  const { id } = req.body;
+    const { id } = req.body;
 
-  // Validate id
-  if (!id) {
-    res.status(401).json({ "error": "Invalid user id" });
-  }
-
-  // Get user balance
-  else {
-    const balance = getUserBalance(id);
-    if (user) {
-      console.info('user balance', balance);
-      res.status(200).json({ "balance": balance });
-    } else {
-      res.status(401).json({ "error": "Invalid user" });
+    // Validate id
+    if (!id) {
+        res.status(401).json({ "error": "Invalid user id" });
     }
-  }
+
+    // Get user balance
+    else {
+        const balance = getUserBalance(id);
+        if (user) {
+            console.info('user balance', balance);
+            res.status(200).json({ "balance": balance });
+        } else {
+            res.status(401).json({ "error": "Invalid user" });
+        }
+    }
 })
 
 // Get recent transactions
 userRouter.get("/transactions", async function (req, res) {
-  const { id } = req.body;
+    const { id } = req.body;
 
-  // Validate id
-  if (!id) {
-    res.status(401).json({ "error": "Invalid user id" });
-  }
-
-
-  // Get recent transactions
-  else {
-    const recentTransactions = getRecentTransactions(id);
-    if (user) {
-      console.info('recentTransactions', recentTransactions);
-      // Get recent transactions
-      res.status(200).json({ "transactions": recentTransactions });
-    } else {
-      res.status(401).json({ "error": "Invalid user" });
+    // Validate id
+    if (!id) {
+        res.status(401).json({ "error": "Invalid user id" });
     }
-  }
+
+
+    // Get recent transactions
+    else {
+        const recentTransactions = getRecentTransactions(id);
+        if (user) {
+            console.info('recentTransactions', recentTransactions);
+            // Get recent transactions
+            res.status(200).json({ "transactions": recentTransactions });
+        } else {
+            res.status(401).json({ "error": "Invalid user" });
+        }
+    }
 
 
 })
