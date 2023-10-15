e7c72467829eb4d0372e61d05a7deea1fb78d1d6
Longqirui Chen <longqirui.chen@mail.utoronto.ca>
2023-10-14 23:14:42 -0400

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,43 +7,86 @@ import Box from '@mui/material/Box';
 //import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 
+function Spawnuser({value,username}) {
+  return (
+    <Paper
+      style={{
+        width: '600px',
+        height: '100px',
+        display: 'flex',
+        //justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        style={{
+          width: '60px',
+          height: '60px',
+          backgroundColor: 'grey',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          border: '2px solid black',
+        }}
+      >
+        {"icon"}
+      </Box>
+      {username+": "+"$"+value}
+    </Paper>
+  );
+}
+function user({username,x,y})
+{
+  return (
+    <Box
+      sx={{
+        position: 'absolute', 
+        left: x, 
+        top: y,
+        display: 'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius: '10px',
+        width: 300,
+        height: 300,
+        backgroundColor: 'lightblue',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+      > {"username"}</Box>
+  );
+}
 export default function Home() {
   return (
-      <Container>
-        {/* Big Header Box */}
-        <Paper
-          style={{
-            padding: '100px',
-            marginBottom: '16px',
-
-          }}
-        >
-          <Typography variant="h4">Dashboard</Typography>
-          {"Total:"}
-        </Paper>
-        {/*small boxes*/}
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6} md={4}>
-            <Paper style={{minHeight: '100px',}}>
-              <Typography variant="h6">Username 1</Typography>
-              {/* Place content for the first small box here */}
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Paper style={{minHeight: '100px',}}>
-              <Typography variant="h6">Username 2</Typography>
-              {/* Place content for the second small box here */}
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Paper style={{minHeight: '100px',}}>
-              <Typography variant="h6">Username 3</Typography>
-              {/* Place content for the third small box here */}
-            </Paper>
-          </Grid>
-          {/* You can add more small boxes as needed */}
-        </Grid>
-      </Container>
+    <>
     
+    <Container
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-start', // Align to the top
+        height: '100vh', // Set the height of the container to the viewport height
+      }}
+    >
+      <Typography variant="h4">
+        Total <br /> $100
+      </Typography>
+    </Container>
+    
+    <Container
+      style={{
+        position: 'absolute',
+        left: '30%', 
+        top: '30%',
+      }}
+      >
+      <Spawnuser value={10} username={"user1"}/>
+      <Spawnuser value={20} username={"user2"}/>
+      <Spawnuser value={30} username={"user3"}/>
+    </Container>
+    </>
   );
 }
